8b75a06c8278192e9127c598c0e605ec98e3bfa1
Scot P. Floess <flossware@gmail.com>
2017-11-13 08:39:09 -0500

    Updating to use github checkout repo

--- bash/flossware.sh
+++ bash/flossware.sh
@@ -1,18 +1,18 @@
-#!/bin/sh 
+#!/bin/sh
 
 # Script to execute as the root user at boot time.
 # You can loose your data or make the system inaccessible
 # if using the wrong commands. You have been warned!
-
+ 
 exec >> /var/log/user.log 2>&1
 
 case "$1" in
 	start)
 		echo "Starting $0"
-                /mnt/md0/bootup.sh /startup.sh	
+                /mnt/md0/root/Development/github/sfloess/alt-f/bash/bootup.sh /root/Development/github/sfloess/alt-f/bash/startup.sh	
 		;;
 	stop)
 	      	echo "Stopping $0"
-                debian -chroot /shutdown.sh
+                debian -chroot /root/Development/github/sfloess/alt-f/bash/shutdown.sh
 		;;
 esac
