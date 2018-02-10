2e15b36eca446acb44d0d844a201a85ebd48555c
root <root@admin-server.localnet>
2018-02-10 15:41:54 -0500

    Adding in autofs

--- bash/startup.sh
+++ bash/startup.sh
@@ -5,9 +5,10 @@
 /etc/init.d/rpcbind         start
 /etc/init.d/inetutils-inetd start
 /etc/init.d/nfs-common      start
-#/etc/init.d/autofs          start
+/etc/init.d/autofs          start
 /etc/init.d/ssh             start
 /etc/init.d/cobblerd        start
 /etc/init.d/apache2         start
+/etc/init.d/puppet-master   start
 
 mount -a
