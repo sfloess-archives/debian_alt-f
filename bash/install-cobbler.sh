812ad09dc9d4482db0721417889f66dea3949ee0
Scot P. Floess <sfloess@redhat.com>
2018-02-11 14:28:17 -0500

    Adjusting for pxe boots

--- bash/install-cobbler.sh
+++ bash/install-cobbler.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get install -y make git apache2 python-yaml python-cheetah python-netaddr python-simplejson python-urlgrabber libapache2-mod-wsgi python-django atftpd yum-utils lsb tftpd-hpa
+apt-get install -y make git apache2 python-yaml python-cheetah python-netaddr python-simplejson python-urlgrabber libapache2-mod-wsgi python-django atftpd yum-utils lsb tftpd-hpa syslinux-common pxelinux
 
 mkdir -p ${HOME}/Development/github/cobbler
 cd ${HOME}/Development/github/cobbler
@@ -15,10 +15,15 @@ a2enmod proxy
 a2enmod proxy_http
 a2enmod rewrite
 
-ln -s /etc/apache2/conf-available/cobbler.conf /etc/apache2/conf-enabled/
-ln -s /etc/apache2/conf-available/cobbler_web.conf /etc/apache2/conf-enabled
+ln -s /etc/apache2/conf-available/cobbler.conf       /etc/apache2/conf-enabled/
+ln -s /etc/apache2/conf-available/cobbler_web.conf   /etc/apache2/conf-enabled
+
 ln -s /usr/local/lib/python2.6/dist-packages/cobbler /usr/lib/python2.6/dist-packages/
 ln -s /usr/local/lib/python2.7/dist-packages/cobbler /usr/lib/python2.7/dist-packages/ 
+
+ln -s /usr/lib/PXELINUX/pxlinux.0                    /usr/lib/syslinux/
+ln -s /usr/lib/syslinux/modules/bios/menu.c32        /usr/lib/syslinux/
+ln -s /usr/lib/syslinux/modules/bios/ldlinux.c32     /usr/lib/syslinux/
 
 chown www-data /var/lib/cobbler/webui_sessions
 
