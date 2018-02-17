cef18b95c0fb1068fe2d14dfc3b9d6f6dbf7c11c
Scot P. Floess <sfloess@redhat.com>
2018-02-16 23:41:05 -0500

    Adding some new functionality for necessary installation of livecd-tools

--- bash/install-cobbler.sh
+++ bash/install-cobbler.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+# ----------------------------------------------------------
+
 apt-get install -y make git apache2 python-yaml python-cheetah python-netaddr python-simplejson python-urlgrabber libapache2-mod-wsgi python-django yum-utils lsb tftpd-hpa syslinux-common pxelinux isolinux genisoimage
 
-mkdir -p ${HOME}/Development/github/cobbler
-cd ${HOME}/Development/github/cobbler
+# ----------------------------------------------------------
+
+DEVELOPMENT_HOME=${HOME}/Development
+GITHUB_HOME=${DEVELOPMENT_HOME}/github
+
+# ----------------------------------------------------------
+
+COBBLER_HOME=${GITHUB_HOME}/cobbler
+
+mkdir -p ${COBBLER_HOME}
+cd ${COBBLER_HOME}
 
 git clone https://github.com/cobbler/cobbler.git
 
@@ -11,11 +22,34 @@ cd cobbler
 
 make install
 
+# ----------------------------------------------------------
+
+LIVECD_TOOLS_HOME=${GITHUB_HOME}/livecd-tools
+
+mkdir -p ${LIVECD_TOOLS_HOME}
+cd ${LIVECD_TOOLS_HOME}
+
+git@github.com:livecd-tools/livecd-tools.git
+
+cd livecd-tools
+
+make install
+
+# ----------------------------------------------------------
+
+cd ${HOME}
+
+# ----------------------------------------------------------
+
 a2enmod proxy
 a2enmod proxy_http
 a2enmod rewrite
 
+# ----------------------------------------------------------
+
 mkdir -p /usr/share/syslinux/
+
+# ----------------------------------------------------------
 
 ln -s /etc/apache2/conf-available/cobbler.conf       /etc/apache2/conf-enabled/
 ln -s /etc/apache2/conf-available/cobbler_web.conf   /etc/apache2/conf-enabled/
@@ -36,9 +70,13 @@ ln -s /usr/lib/syslinux/modules/bios/libcom32.c32    /usr/share/syslinux/
 ln -s /usr/lib/syslinux/modules/bios/libutil.c32     /usr/share/syslinux/
 
 ln -s /usr/bin/genisoimage                           /usr/bin/mkisofs
+ln -s /usr/bin/genisoimage                           /usr/bin/livecd-iso-to-pxeboot
 
+# ----------------------------------------------------------
 
 chown www-data /var/lib/cobbler/webui_sessions
+
+# ----------------------------------------------------------
 
 SECRET_KEY=$(python -c 'import re;from random import choice; import sys; sys.stdout.write(re.escape("".join([choice("abcdefghijklmnopqrstuvwxyz0123456789^&*(-_=+)") for i in range(100)])))') 
 
@@ -46,7 +84,11 @@ sed -i "s/^SECRET_KEY = .*/SECRET_KEY = '${SECRET_KEY}'/" /usr/local/share/cobbl
 sed -i "s/^SECRET_KEY = .*/SECRET_KEY = '${SECRET_KEY}'/" /usr/local/lib/python2.6/dist-packages/cobbler/web/settings.py
 sed -i "s/^SECRET_KEY = .*/SECRET_KEY = '${SECRET_KEY}'/" /usr/local/lib/python2.7/dist-packages/cobbler/web/settings.py
 
+# ----------------------------------------------------------
+
 sed -i "s/^server: 127.0.0.1/server: 192.168.168.31/" /etc/cobbler/settings
+
+# ----------------------------------------------------------
 
 sed -i 's/--secure/--secure --ipv4/' /etc/defaults/tftpd-hpa
 
@@ -54,7 +96,11 @@ echo "Enter password for cobbler..."
 
 htdigest /etc/cobbler/users.digest "Cobbler" cobbler
 
+# ----------------------------------------------------------
+
 service apache2   restart
 service cobblerd  restart
 service tftpd-hpa restart
+
+# ----------------------------------------------------------
 
