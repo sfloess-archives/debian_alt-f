e930358318f0a9d8f6e502897d4028f20ba1aa7d
Scot P. Floess <sfloess@nc.rr.com>
2019-11-03 15:33:54 -0500

    wip

--- bash/install-prereq.sh
+++ bash/install-prereq.sh
@@ -8,7 +8,7 @@ apt-get install -y git make python3-dev python3-setuptools python3-cheetah opens
 apt-get install apache2-dev
 apt-get install yum-utils tftpd-hpa
 apt-get install libapache2-mod-wsgi-py3
-apt-get install postfix alpine lsb-release locales genders sudo
+apt-get install postfix alpine lsb-release locales genders sudo genisoimage
 
 # ------------------------
 
@@ -17,6 +17,10 @@ pip2 install netaddr yaml
 
 ln -s /usr/lib/PXELINUX/pxelinux.0 /usr/lib/syslinux/pxelinux.0
 ln -s /usr/lib/selinux/modules/bios/menu.c32 /usr/lib/syslinux/menu.c32
+ln -s /usr/lib/ISOLINUX/isolinux.bin /usr/lib/syslinux/isolinux.bin
+ln -s /usr/lib/syslinux/menu.c32 /var/lib/cobbler/loaders/menu.c32
+ln -s /usr/lib/selinux/modules/bios/chain.c32 /usr/lib/syslinux/chain.c32
+ln -s /usr/bin/genisoimage /usr/bin/mkisofs
 
 # ------------------------
 
