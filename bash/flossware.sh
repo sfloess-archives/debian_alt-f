79e0c328e58281800a5216e658ee569e327db98e
Scot P. Floess <sfloess@nc.rr.com>
2019-11-09 07:13:00 +0000

    latest

--- bash/flossware.sh
+++ bash/flossware.sh
@@ -9,10 +9,10 @@ exec >> /var/log/user.log 2>&1
 case "$1" in
 	start)
 		echo "Starting $0"
-                /mnt/sda2/Development/github/sfloess/alt-f/bash/bootup.sh /Development/github/sfloess/alt-f/bash/startup.sh	
+                /mnt/sda2/opt/development/github/sfloess/debian_alt-f/bash/bootup.sh /opt/development/github/sfloess/debian_alt-f/bash/startup.sh	
 		;;
 	stop)
 	      	echo "Stopping $0"
-                debian -chroot /mnt/sda2/Development/github/sfloess/alt-f/bash/shutdown.sh
+                debian -chroot /mnt/sda2/opt/development/github/sfloess/debian_alt-f/bash/shutdown.sh
 		;;
 esac
